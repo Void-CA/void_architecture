87eab5b560730018047c9fed8926216f0f65ffdb
Void-CA <castilloari282@gmail.com>
2026-03-19 14:03:53 -0600

    feat(generate): enhance React feature generation with directory and file creation

--- src/cli/generate/feature/stacks/mod.rs
+++ src/cli/generate/feature/stacks/mod.rs
@@ -1,2 +1,26 @@
 pub mod react;
 pub mod tauri;
+
+use std::fs;
+
+enum FileType {
+    Dir(String),
+    File(String),
+}
+
+impl FileType {
+    fn dir(path: &str) -> Self {
+        FileType::Dir(path.to_string())
+    }
+
+    fn file(path: &str) -> Self {
+        FileType::File(path.to_string())
+    }
+
+    fn create(&self) {
+        match self {
+            FileType::Dir(path) => { fs::create_dir_all(path).unwrap(); }
+            FileType::File(path) => { fs::File::create(path).unwrap(); }
+        }
+    }
+}
--- src/cli/generate/feature/stacks/react.rs
+++ src/cli/generate/feature/stacks/react.rs
@@ -1,3 +1,37 @@
+use super::FileType;
+
 pub fn execute(feature_name: &str) {
-    println!("Generating React feature: {}", feature_name);
+    let feature_path = format!("src/features/{}", feature_name);
+
+    let dirs = vec![
+        FileType::Dir(format!("{}/components", feature_path)),
+        FileType::Dir(format!("{}/hooks", feature_path)),
+        FileType::Dir(format!("{}/data", feature_path)),
+    ];
+
+    let files = vec![
+        // root
+        FileType::File(format!("{}/index.ts", feature_path)),
+        FileType::File(format!("{}/service.ts", feature_path)),
+        FileType::File(format!("{}/queryKeys.ts", feature_path)),
+
+        // hooks
+        FileType::File(format!("{}/hooks/index.ts", feature_path)),
+        FileType::File(format!("{}/hooks/queries.ts", feature_path)),
+
+        // data
+        FileType::File(format!("{}/data/dto.ts", feature_path)),
+        FileType::File(format!("{}/data/model.ts", feature_path)),
+        FileType::File(format!("{}/data/mapper.ts", feature_path)),
+    ];
+
+    // 1. Crear directorios
+    for dir in dirs {
+        dir.create();
+    }
+
+    // 2. Crear archivos
+    for file in files {
+        file.create();
+    }
 }
